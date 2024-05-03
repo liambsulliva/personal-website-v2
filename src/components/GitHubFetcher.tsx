fed46ac6f272e9410777134b9cccd089754aa491
Liam <{ID}+{username}@users.noreply.github.com>
2024-05-03 01:59:14 -0400

    fixed skills table mobile responsiveness

--- src/components/GitHubFetcher.tsx
+++ src/components/GitHubFetcher.tsx
@@ -20,6 +20,7 @@ const GitHubFetcher = (): JSX.Element => {
     fetchRepos();
   }, []);
 
+  // TODO: Skip forked repos, personal-website-v2, and special repo liambsulliva
   return (
     <div className="flex-row rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       {repos.map((repo, index) => (
@@ -29,6 +30,7 @@ const GitHubFetcher = (): JSX.Element => {
             </a>
             <p className="mb-3 font-normal text-gray-400">{repo.description}</p>
             <div className="flex flex-column gap-2">
+              {/* Add button animations here to match design pattern */}
               <a href={repo.html_url} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" target="_blank">
                   View Project
                   <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
