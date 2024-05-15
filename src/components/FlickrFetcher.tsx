f5d1cff221cccbfe1262111d137cadc1b00c55d5
Liam <{ID}+{username}@users.noreply.github.com>
2024-05-15 00:13:41 -0400

    gallery rework

--- src/components/FlickrFetcher.tsx
+++ src/components/FlickrFetcher.tsx
@@ -8,6 +8,30 @@ interface FlickrFetcherProps {
 // TODO: Lazy load pages at a time so we can animate photos in as they load
 const FlickrFetcher: React.FC<FlickrFetcherProps> = ({ apiKey, userId }) => {
     const [flickrData, setFlickrData] = useState<{ src: string, title: string }[]>([]);
+    const [loadedImages, setLoadedImages] = useState(0);
+    const [isRandomized, setIsRandomized] = useState(false);
+    const [selectedPhoto, setSelectedPhoto] = useState<{ src: string, title: string } | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Randomize the order of flickrData array
+    const shuffleArray = (array: { src: string, title: string }[]) => {
+      const shuffledArray = [...array];
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      return shuffledArray;
+    };
+
+    const openModal = (photo: { src: string, title: string }) => {
+      setSelectedPhoto(photo);
+      setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+      setSelectedPhoto(null);
+      setIsModalOpen(false);
+    };
   
     useEffect(() => {
       const fetchFlickrData = async () => {
@@ -23,38 +47,88 @@ const FlickrFetcher: React.FC<FlickrFetcherProps> = ({ apiKey, userId }) => {
             const pageResponse = await fetch(pageUrl);
             const pageData = await pageResponse.json();
             if (pageData.stat === 'ok') {
+                const screenWidth = window.innerWidth;
+                let photoQuality;
+                if (screenWidth < 400) {
+                  photoQuality = 'n';
+                } else if (screenWidth < 640) {
+                  photoQuality = 'z';
+                } else {
+                  photoQuality = 'b';
+                }
                 for (let j = 0; j < pageData.photos.photo.length; j++) {
                     const photo = pageData.photos.photo[j];
-                    const src = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+                    const src = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${photoQuality}.jpg`;
                     const title = photo.title;
+                    console.log(src);
                     photos.push({ src, title });
                 }
             } else {
                 console.error(`Error fetching Flickr data for page ${i}, ${pageData.message}`);
             }
         }
-        localStorage.setItem('flickrData', JSON.stringify(photos));
+        photos = shuffleArray(photos);
         setFlickrData(photos);
         } else {
           console.error(`Error fetching Flickr data ${data.message}`);
         }
       };
-      if (localStorage.getItem('flickrData') === null) {
-        fetchFlickrData();
-      } else {
-        setFlickrData(JSON.parse(localStorage.getItem('flickrData') || '[]'));
-        //console.log('Flickr data loaded from local storage');
-      }
+      fetchFlickrData();
     }, []);
 
     return (
-      <div className="portfolio-container sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 p-12 pt-0">
-        {flickrData.map((photo, index) => (
-          <div key={index} className={`portfolio-wrapper`}>
-            <img src={photo.src} alt={photo.title} className="portfolio-element h-auto w-full p-2.5" />
+      <>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 m-4">
+          <div className="grid gap-4 h-auto">
+          {flickrData.slice(0, flickrData.length/4).map((photo, index) => (
+            <div key={index}>
+            <button onClick={() => openModal(photo)}>
+              <img className="h-auto w-full rounded-lg" src={photo.src} alt={photo.title} />
+            </button>
+            </div>
+          ))}
           </div>
-        ))}
+          <div className="grid gap-4 h-auto">
+          {flickrData.slice(flickrData.length/4, 2*flickrData.length/4).map((photo, index) => (
+            <div key={index}>
+            <button onClick={() => openModal(photo)}>
+              <img className="h-auto w-full rounded-lg" src={photo.src} alt={photo.title} />
+            </button>
+            </div>
+          ))}
+          </div>
+          <div className="grid gap-4 h-auto">
+          {flickrData.slice(2*flickrData.length/4, 3*flickrData.length/4).map((photo, index) => (
+            <div key={index}>
+            <button onClick={() => openModal(photo)}>
+              <img className="h-auto w-full rounded-lg" src={photo.src} alt={photo.title} />
+            </button>
+            </div>
+          ))}
+          </div>
+          <div className="grid gap-4 h-auto">
+          {flickrData.slice(3*flickrData.length/4, flickrData.length).map((photo, index) => (
+            <div key={index}>
+            <button onClick={() => openModal(photo)}>
+              <img className="h-auto w-full rounded-lg" src={photo.src} alt={photo.title} />
+            </button>
+            </div>
+          ))}
+        </div>
       </div>
+      {isModalOpen && selectedPhoto && (
+        <div onClick={closeModal} className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black opacity-75"></div>
+          <div className="relative z-10 p-4">
+            <img
+              className="h-full rounded-lg"
+              src={selectedPhoto.src}
+              alt={selectedPhoto.title}
+            />
+          </div>
+        </div>
+      )}
+      </>
     );
   };
 
