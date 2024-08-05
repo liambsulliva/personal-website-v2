b1c368f04501632992f40df29f4a4165abc0f07f
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-08-05 16:32:28 -0400

    add vercel as adapter type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind(), react(), astroI18next()]
+  output: "hybrid",
+  integrations: [tailwind(), react(), astroI18next()],
+  adapter: vercel(),
 });
